72394a133411628c75f6b755627f61e21f4b6a46
Azeem Ghafoor <azeemsulehri009@gmail.com>
2023-08-21 14:44:41 +0500

    changes

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use('/preferences', preferenceRoutes);
 app.use('/notifications', notificationRoutes);
 app.use(errorHandler);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
